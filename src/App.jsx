7485f7ece60939fdfdd7dda550e40415ca761ac9
Gautam Chatterjee <gautam.kvafs@gmail.com>
2026-08-21 17:27:13 +0530

    Update WhatsApp contact number

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import HomePage from "./pages/HomePage";
 const assetUrl = (path) => `${import.meta.env.BASE_URL}${path}`;
 
 const siteConfig = {
-  whatsappNumber: "919620417344",
+  whatsappNumber: "919995112919",
   whatsappMessage:
     "Hello SWOTEDGE, I would like to discuss an event, activation, branding, or experiential marketing project.",
 };
